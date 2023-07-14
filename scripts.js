4975f5316a79dffa9e98129e770d12dacb8662d1
Anastasiia Yemelianova <nastia.yemelianova@gmail.com>
2023-07-14 12:55:33 +0300

    finish adaptivity

--- scripts.js
+++ scripts.js
@@ -2,27 +2,21 @@ const humburgerBtn = document.querySelector('.humburger-btn');
 const humburgerContainer = document.querySelector('.hamburger-container');
 const contactsPaginationBtns = document.querySelectorAll('.contacts-pagination');
 const contactsPaginationList = document.querySelector('.contacts-list').children;
-
-const servicesPaginationBtns = document.querySelectorAll('.services-pagination');
+const servicesPaginationBtns = document.querySelectorAll('.pagination-services-btn');
 const servicesPaginationList = document.querySelector('.services-list').children;
+const technologiesPaginationBtns = document.querySelectorAll('.pagination-technologies-btn');
+const technologiesPaginationList = document.querySelector('.technologies-list').children;
+const processesPaginationBtns = document.querySelectorAll('.pagination-processes-btn ');
+const processesPaginationList = document.querySelector('.process-list').children;
 
 let currentContact = 0;
-// let currentService = 0;
+let currentService = 0;
+let currentTechnology = 0;
+let currentProcess = 0;
 
 const handleEvent = () => {
     humburgerContainer.classList.toggle('hamburger-container_visible');
     humburgerBtn.classList.toggle('humburger-btn_open');
-}
-
-const handlePrevContact = () => {
-    contactsPaginationList[currentContact].style.display = "none";
-    if (currentContact === 0) {
-        currentContact = 2;
-        contactsPaginationList[currentContact].style.display = "";
-    } else {
-        contactsPaginationList[currentContact - 1].style.display = "";
-        currentContact--;
-    }
 }
 
 const handleNextContact = () => {
@@ -36,9 +30,45 @@ const handleNextContact = () => {
     }
 }
 
-const handleDoubleServices = () => {
-    for (let i = 0; i < servicesPaginationList.length; i++) {
-        servicesPaginationList[i].classList.toggle('services-list__item_hidden');
+// const handleDoubleServices = () => {
+//     for (let i = 0; i < servicesPaginationList.length; i++) {
+//         servicesPaginationList[i].classList.toggle('services-list__item_hidden');
+//     }
+// }
+
+const handleNextSingleService = () => {
+    servicesPaginationList[currentService].style.display = "none";
+
+    if (currentService === 3) {
+        currentService = 0;
+        servicesPaginationList[currentService].style.display = "";
+    } else {
+        servicesPaginationList[currentService + 1].style.display = "";
+        currentService++;
+    }
+}
+
+const handleNextTechnology = () => {
+    technologiesPaginationList[currentTechnology].style.display = "none";
+
+    if (currentTechnology === 3) {
+        currentTechnology = 0;
+        technologiesPaginationList[currentTechnology].style.display = "";
+    } else {
+        technologiesPaginationList[currentTechnology + 1].style.display = "";
+        currentTechnology++;
+    }
+}
+
+const handleNextProcess = () => {
+    processesPaginationList[currentProcess].style.display = "none";
+
+    if (currentProcess === 4) {
+        currentProcess = 1;
+        processesPaginationList[currentProcess].style.display = "";
+    } else {
+        processesPaginationList[currentProcess + 1].style.display = "";
+        currentProcess++;
     }
 }
 
@@ -49,33 +79,67 @@ const displayContact = () => {
 
     for (elem of contactsPaginationBtns) {
         elem.classList.toggle('contacts-pagination_visible');
-        if (elem.classList.contains('contacts-pagination_prev')) {
-            elem.addEventListener("click", handlePrevContact);
-        }
-        else {
-            elem.addEventListener("click", handleNextContact);
-        }
+        elem.addEventListener("click", handleNextContact);
     }
 }
 
 const displayServices = () => {
-    for (let i = servicesPaginationList.length - 1; i > servicesPaginationList.length / 2 - 1; i--) {
-        servicesPaginationList[i].classList.toggle('services-list__item_hidden');
+    // for two cards
+    // for (let i = servicesPaginationList.length - 1; i > servicesPaginationList.length / 2 - 1; i--) {
+    //     servicesPaginationList[i].classList.toggle('services-list__item_hidden');
+    // }
+
+    // for (elem of servicesPaginationBtns) {
+    //     if (elem.classList.contains('pagination-btn_prev')) {
+    //         elem.addEventListener("click", handleDoubleServices);
+    //     }
+    //     else {
+    //         elem.addEventListener("click", handleDoubleServices);
+    //     }
+    // }
+
+    // for one card
+    for (let i = 1; i < servicesPaginationList.length; i++) {
+        servicesPaginationList[i].style.display = "none";
     }
 
     for (elem of servicesPaginationBtns) {
-        if (elem.classList.contains('services-pagination_prev')) {
-            elem.addEventListener("click", handleDoubleServices);
-        }
-        else {
-            elem.addEventListener("click", handleDoubleServices);
-        }
+        elem.classList.toggle('pagination-btn_visible');
+        elem.addEventListener("click", handleNextSingleService);
     }
 }
+
+const displayTechnologies = () => {
+    for (let i = 1; i < technologiesPaginationList.length; i++) {
+        technologiesPaginationList[i].style.display = "none";
+    }
+
+    for (elem of technologiesPaginationBtns) {
+        elem.classList.toggle('pagination-btn_visible');
+        elem.addEventListener("click", handleNextTechnology);
+    }
+}
+
+const displayProcess = () => {
+    for (let i = 2; i < processesPaginationList.length; i++) {
+        processesPaginationList[i].style.display = "none";
+    }
+
+    for (elem of processesPaginationBtns) {
+        elem.classList.toggle('pagination-btn_visible');
+        elem.addEventListener("click", handleNextProcess);
+    }
+}
+
+document.getElementById("form").addEventListener("click", function(e) {
+    e.preventDefault();
+});
 
 humburgerBtn.addEventListener('click', handleEvent);
 
 if (window.screen.width <= 970) {
     displayContact();
     displayServices();
+    displayTechnologies();
+    displayProcess();
 }
